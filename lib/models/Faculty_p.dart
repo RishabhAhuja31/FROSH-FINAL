84843dbf9724d611f2a183f8a973d3860648b78b
abhimanyu-mehta26 <94471929+abhimanyu-mehta26@users.noreply.github.com>
2022-08-28 19:49:14 +0530

    Update Faculty_p.dart

--- lib/models/Faculty_p.dart
+++ lib/models/Faculty_p.dart
@@ -15,5 +15,5 @@ List<DataModel> dataList = [
   DataModel("Dr. Mandeep Singh", "https://i.imgur.com/TthaGLg.jpg",''),
   DataModel("Dr. Hemdutt Joshi", "https://i.imgur.com/zB8X5hl.jpg", ''),
   DataModel("Dr. Devendar kumar", "https://i.imgur.com/1a9T684.jpg", ''),
-  DataModel("Dr. Gitanjali Chandwani Manocha", "https://i.imgur.com/gDlIVjp.jpg", ''),
+  DataModel("Dr. Gitanjali Chandwani Manocha", "https://i.imgur.com/AlkFiRc.jpg", ''),
 ];
